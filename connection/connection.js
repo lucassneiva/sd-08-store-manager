5e95d98051e8c1f8d9fa9d47d4bd909040c0973e
Thiago Ferreira <thiagocristhian@gmail.com>
2021-06-01 20:42:25 -0300

    Alterando connection do localhost para mongoDB

--- connection/connection.js
+++ connection/connection.js
@@ -1,10 +1,10 @@
 const { MongoClient } = require('mongodb');
 
 // URL PARA TESTAR LOCALMENTE O PROJETO
-const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+// const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
 
 // URL PARA RODAR OS TESTES DO REPOSITÓRIO
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
 
 const DB_NAME = 'StoreManager';
 
