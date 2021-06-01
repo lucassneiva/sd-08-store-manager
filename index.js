b3b8003c0a787450ae8eaf304aa866ff26aa7ab3
Bruno Wedley <brunowedley27@gmail.com>
2021-06-01 14:31:30 -0300

    primeiro commit com organização inicial da estrutura do projeto

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+const PORT = 3000;
+
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.listen(process.env.PORT || PORT, () => console.log('Fala que eu te escuto!'));
