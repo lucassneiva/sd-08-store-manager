9b1903971c5bbea5d1cf944fa59d90ddec3e25ca
Felipe Segovia <felipe.vsegovia@gmail.com>
2021-06-01 14:56:16 -0300

    Store Manager Project - Initial Commit

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+const express = require(express);
+
+const app = express();
+const port = 3001;
+
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log('App ouvindo a porta 3001!');
+});
+
+
+
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
