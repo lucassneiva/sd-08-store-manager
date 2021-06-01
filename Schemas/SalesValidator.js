3b70376a4a771b57c4d699535ecc27988c8f2e31
Ruan Miguel <ruanmiguelps@gmail.com>
2021-06-01 14:46:01 -0300

    req 1-8

--- Schemas/SalesValidator.js
+++ Schemas/SalesValidator.js
@@ -1,8 +1,11 @@
 const { ObjectId } = require('bson');
 const number = 0;
+const {uniqueValue, findById} = require('../models/Products');
 
 const salesValidator =   (salesArr) => {
   const data = salesArr.map(item => item.quantity);
+
+
   if(data.some(item => item <= number))  return {err:{
     code:'invalid_data',
     message:'Wrong product ID or invalid quantity'
@@ -14,15 +17,20 @@ const salesValidator =   (salesArr) => {
     message:'Wrong product ID or invalid quantity'
   }
   };
-    
+
+  
 
   return {};
 };
 
 
+const validadeStock  = (salesArr) =>  {
+  const mapped = salesArr.map(item => item.productId);
+  console.log(mapped);
 
+}; 
 
 module.exports = {
   salesValidator,
-    
+  validadeStock
 };
