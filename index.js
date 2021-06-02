c557cf3cc6232fea324a03e384157a769a7e436c
Bruno Wesley <bruno.wesley77@gmail.com>
2021-06-02 17:47:04 -0300

    commit inicial

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+const express = require('express');
+const port = 3000;
+
+const app = express();
+
+app.use(express.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.listen(port, () => console.log('Online'));
