8b715be22ec05364dd4c78e4df719b195987030d
Jeferson Schimuneck <jefersonschimuneck@gmail.com>
2021-06-02 19:11:28 -0300

    Complete requirement 1

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-// não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
-  response.send();
-});
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const controller = require('./controllers/productController');
+const errorMiddleware = require('./middlewares/error');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', (_request, response) => {response.send();});
+app.get('/products', controller.readAll);
+app.post('/products', controller.create);
+
+app.use(errorMiddleware);
+
+const LOCALHOST = 3000;
+const port = process.env.PORT || LOCALHOST;
+
+app.listen(port, () => { console.log(`Listening on port ${port}`); });
---
+++ services/productService.js
@@ -0,0 +1,41 @@
+const Joi = require('@hapi/joi');
+
+const model = require('../models/productModel');
+
+const MIN_NAME_LENGTH = 5;
+const validateProduct = Joi.object({
+  name: Joi.string().min(MIN_NAME_LENGTH).required(),
+  quantity: Joi.number().min(1).required(),
+});
+
+const create = async (name, quantity) => {
+  const { error } = validateProduct.validate({ name, quantity });
+  
+  if (error) { 
+    return {status: 422, code: 'invalid_data', error};
+  };
+
+  const allProducts = await readAll();
+  const isNameUsed = allProducts.some(product => product.name === name);
+
+  if (isNameUsed) {
+    return {
+      status: 422, code: 'invalid_data', error: { message: 'Product already exists' }
+    };
+  }
+
+  const newProduct = model.create(name, quantity);
+
+  return newProduct;
+};
+
+const readAll = async () => {
+  const products = await model.readAll();
+
+  return products;
+};
+
+module.exports = {
+  create,
+  readAll
+};
