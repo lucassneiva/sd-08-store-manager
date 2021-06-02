e4ae95bd37f3bcf484bcf9d3e13fe880d6b2c3df
Bruno Martins <bruno.martins217@gmail.com>
2021-06-02 15:55:14 -0300

    refactor

--- controller/productController.js
+++ controller/productController.js
@@ -4,47 +4,31 @@ const { checkProduct } = require('../middlewares/index');
 
 const createProduct = async (req, res) => {
   const {name, quantity} = req.body;
-  try {
-    checkProduct(req, res);
-    const data = await products.createProduct(name, quantity);
-    if(data.err) return res.status(error).json(data);
-    res.status(update).json(data);
-  } catch (error) {
-    console.log(error);
-  }
+  checkProduct(req, res);
+  const data = await products.createProduct(name, quantity);
+  if(data.err) return res.status(error).json(data);
+  res.status(update).json(data);
 };
 
 const getAll = async(_req, res) => {
-  try {
-    const data = await products.getAll();
-    res.status(success).json(data);
-  } catch (error) {
-    res.status(error).json(error);
-  }
+  const data = await products.getAll();
+  res.status(success).json(data);
 };
 
 const getById = async(req, res) => {
   const { id } = req.params;
-  try {
-    const data = await products.getById(id);
-    if(data.err) return res.status(error).json(data);
-    res.status(success).json(data);
-  } catch (error) {
-    console.log(error.message);
-  }
+  const data = await products.getById(id);
+  if(data.err) return res.status(error).json(data);
+  return res.status(success).json(data);
 };
 
 const updateProduct = async(req, res) => {
   const { id } = req.params;
   const{ name, quantity } = req.body;
-  try {
-    checkProduct(req, res);
-    const data = await products.updateProduct(id, name, quantity);
-    if(data.err) return res.status(error).json(data);
-    res.status(success).json(data);
-  } catch (error) {
-    console.log(error.message);
-  }
+  checkProduct(req, res);
+  const data = await products.updateProduct(id, name, quantity);
+  if(data.err) return res.status(error).json(data);
+  res.status(success).json(data);
 };
 
 module.exports = {
