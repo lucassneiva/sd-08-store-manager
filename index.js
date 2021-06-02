a4fc87d1459b21379f5206f0da43c01dd5b54768
Rita Jeveaux <ritajeveaux@gmail.com>
2021-06-02 16:59:55 -0300

    Iniciando Projeto

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+const express = require('express');
+const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
+});
+
+app.listen(PORT, () => {
+  console.log('Online');
 });
