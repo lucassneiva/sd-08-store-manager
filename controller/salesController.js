37c0e680a06c23d286711a3428da926e2aca4f6b
Bruno Martins <bruno.martins217@gmail.com>
2021-06-03 15:04:41 -0300

    fix: Desafio 5

--- controller/salesController.js
+++ controller/salesController.js
@@ -5,7 +5,7 @@ const { checkSale } = require('../middlewares/checkSale');
 const addSale = async (req, res) => {
   checkSale(req, res);
   const data = await sales.addSale(req);
-  return data.err ? res.status(error).json(data): res.status(success).json(data);
+  return data.err ? res.status(error).json(data) : res.status(success).json(data);
 };
 
 module.exports = {
--- model/salesModel.js
+++ model/salesModel.js
@@ -3,7 +3,7 @@ const connection = require ('../data/connection');
 async function addSale(req){
   const { body } = req;
   const data = await connection().then((db) =>
-    db.connection('sales').insertOne({itensSold: body}));
+    db.collection('sales').insertOne({itensSold: body}));
   return {
     _id: data.insertedId,
     itensSold: body
