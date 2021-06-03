bd5b5a51c6669fb36e8a1733922d5ddadb98e98d
Eduardo <pizorno@gmail.com>
2021-06-03 11:33:27 -0300

    Fix lint errors

--- controllers/Products.js
+++ controllers/Products.js
@@ -1,5 +1,5 @@
 const models = require('../models/Products');
-const { name, quantP, exist, noexist } = require('../services');
+const { nameCheck, quantP, exist, noexist } = require('../services');
 const { Router } = require('express');
 const OK = '200';
 const CREATED = '201';
@@ -11,7 +11,7 @@ productsController.get('/', async (_req, res) => {
   res.status(OK).json({ products });
 });
 
-productsController.post('/', name, quantP, exist, async (req, res) => {
+productsController.post('/', nameCheck, quantP, exist, async (req, res) => {
   const { name, quantity } = req.body;
   console.log(name, quantity);
   const product = await models.create(name, quantity);
@@ -28,17 +28,16 @@ productsController.delete('/:id', noexist, async (req, res) => {
   const { id } = req.params;
   const product = await models.getProduct(id);
   const response = await models.del(id);
-  response.result.ok ? res.status(OK).json(product) : '';
+  response.result.ok ? res.status(OK).json(product) : null;
 });
 
-productsController.put('/:id', name, quantP, async (req, res) => {
+productsController.put('/:id', nameCheck, quantP, async (req, res) => {
   let result = '';
   const { id } = req.params;
   const { name, quantity } = req.body;
   const product = await models.update(id, name, quantity);
-  product.result.ok ? result = await models.getProduct(id): '';
+  product.result.ok ? (result = await models.getProduct(id)) : '';
   res.status(OK).json(result);
 });
 
-
-module.exports = productsController ;
+module.exports = productsController;
--- services/index.js
+++ services/index.js
@@ -2,69 +2,70 @@ const { findName, getProduct } = require('../models/Products');
 const { getSale } = require('../models/sales');
 const ERROR = 422;
 
-const name = (req, res, next) => {
-  const MIN_NAME_LENGHT = 5;
+const nameCheck = (req, res, next) => {
+  const MIN_NAME_LENGTH = 5;
   const { name } = req.body;
-  if (typeof name !== 'string') return res.status(ERROR)
-    .json({
+  if (typeof name !== 'string')
+    return res.status(ERROR).json({
       err: {
         code: 'invalid_data',
         message: '"name" must be a string',
       },
     });
 
-  if ((name === '') || (name === undefined) || (name.length < MIN_NAME_LENGHT)) {
+  if (name === '' || name === undefined || name.length < MIN_NAME_LENGTH) {
     return res.status(ERROR).json({
       err: {
         code: 'invalid_data',
         message: '"name" length must be at least 5 characters long',
       },
     });
-  };
+  }
   next();
 };
 
 const quantP = (req, res, next) => {
   const { quantity } = req.body;
   const zero = 0;
-  if (typeof quantity !== 'number') return res.status(ERROR)
-    .json({
+  if (typeof quantity !== 'number')
+    return res.status(ERROR).json({
       err: {
         code: 'invalid_data',
         message: '"quantity" must be a number',
       },
     });
-  if ((+quantity < 1) || (+quantity == zero)) return res.status(ERROR).json({
-    err: {
-      code: 'invalid_data',
-      message: '"quantity" must be larger than or equal to 1',
-    },
-  });
+  if (+quantity < 1 || +quantity == zero)
+    return res.status(ERROR).json({
+      err: {
+        code: 'invalid_data',
+        message: '"quantity" must be larger than or equal to 1',
+      },
+    });
   next();
 };
 
 const quantS = (req, res, next) => {
-  const itensSold  = req.body;
+  const itensSold = req.body;
   const zero = 0;
-  if (typeof itensSold[0].quantity !== 'number') return res.status(ERROR)
-    .json({
-      err: {
-        code: 'invalid_data',
-        message: 'Wrong product ID or invalid quantity',
-      },
-    });
-  if ((+itensSold[0].quantity < 1) || (+itensSold[0].quantity == zero)) {
+  if (typeof itensSold[0].quantity !== 'number')
     return res.status(ERROR).json({
       err: {
         code: 'invalid_data',
         message: 'Wrong product ID or invalid quantity',
       },
     });
-  };
+  if (+itensSold[0].quantity < 1 || +itensSold[0].quantity == zero) {
+    return res.status(ERROR).json({
+      err: {
+        code: 'invalid_data',
+        message: 'Wrong product ID or invalid quantity',
+      },
+    });
+  }
   next();
 };
 
-const exist = async(req, res, next) => {
+const exist = async (req, res, next) => {
   const { name } = req.body;
   let check = await findName(name);
   if (check) {
@@ -78,7 +79,7 @@ const exist = async(req, res, next) => {
   next();
 };
 
-const noexist = async(req, res, next) => {git c
+const noexist = async (req, res, next) => {
   const { id } = req.params;
   let check = await getProduct(id);
   if (!check) {
@@ -92,7 +93,7 @@ const noexist = async(req, res, next) => {git c
   next();
 };
 
-const noexistS = async(req, res, next) => {
+const noexistS = async (req, res, next) => {
   const { id } = req.params;
   let check = await getSale(id);
   if (!check) {
@@ -106,11 +107,11 @@ const noexistS = async(req, res, next) => {
   next();
 };
 
-module.exports = { 
-  name,
+module.exports = {
+  nameCheck,
   quantP,
   exist,
   noexist,
   quantS,
-  noexistS
+  noexistS,
 };
