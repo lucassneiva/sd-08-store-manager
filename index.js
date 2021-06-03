af10aa18f4393663dd20150ec395714333d6355d
Aline Debastiani <debastianialine@gmail.com>
2021-06-03 01:33:30 -0300

    Revert "cumpre requisito 1"
    This reverts commit b7d86bf67fab309731c05fcc66fa8e429b9fdfb9.

--- index.js
+++ index.js
@@ -1,19 +1,4 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-
-const Products = require('./controllers/Products');
-
-const app = express();
-
-app.use(bodyParser.json());
-
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
-
-app.use('/products', Products);
-
-const PORT = 3000;
-
-app.listen(PORT, () => { console.log(`Listening on port ${PORT}`); });
