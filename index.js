91b701ab53a4a01817a06046b0fb9c53c560e195
Rita Jeveaux <ritajeveaux@gmail.com>
2021-06-03 18:55:00 -0300

    Requisito 1

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+// const Products = require('./controllers/Products');
+// const Sales = require('./controllers/Sales');
+
+const app = express();
 const PORT = '3000';
+
+app.use(bodyParser.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+// app.post('/products', Products.add);
+// app.get('/products', Products.getAll);
+// app.get('/products/:id', Products.getById);
+// app.put('/products/:id', Products.updateById);
+// app.delete('/products/:id', Products.deleteById);
+
+// app.post('/sales', Sales.add);
+// app.get('/sales', Sales.getAll);
+// app.get('/sales/:id', Sales.getById);
+// app.put('/sales/:id', Sales.updateById);
+// app.delete('/sales/:id', Sales.deleteById);
 
 app.listen(PORT, () => {
   console.log('Online');
