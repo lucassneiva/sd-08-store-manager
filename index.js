17088eb7dbff59063802f8381236a630141bff3c
Edson Araujo <araujo.barros@matematica.ufrj.br>
2021-06-04 15:36:10 -0300

    Requisito 3

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ const CREATED = 201;
 const OK = 200;
 const ID_LENGTH = 24;
 
-// 1 - Crie um endpoint para o cadastro de produtos
 
+// 1 - Crie um endpoint para o cadastro de produtos
 
 const existProduct = async ( name ) => {
   const db = await connection();
@@ -79,6 +79,49 @@ const addProduct = async (req, res) => {
 
 app.post('/products', rescue(async (req, res) => {
   const end = await addProduct(req, res);
+  return end;
+}));
+
+// 3 - Crie um endpoint para atualizar um produto
+
+const updateProduct = async (req, res) => {
+  const { body, params } = req;
+  const quantity = validAmount(body.quantity);
+  // if (await existProduct(body['name'])) {
+  //   return res.status(UNPROCESSABE_ENTITY).json({err: {
+  //     code: 'invalid_data',
+  //     message: 'Product already exists'
+  //   }});
+  // } 
+  if (!validLength(body['name'])) {
+    return res.status(UNPROCESSABE_ENTITY).json({err: {
+      code: 'invalid_data',
+      message: '"name" length must be at least 5 characters long'
+    }});
+  } if (quantity === 'IsNaN' ) {
+    return res.status(UNPROCESSABE_ENTITY).json({err: {
+      code: 'invalid_data',
+      message: '"quantity" must be a number'
+    }});
+  } if (quantity === 'IsLessOrEqual0') {
+    return res.status(UNPROCESSABE_ENTITY).json({err: {
+      code: 'invalid_data',
+      message: '"quantity" must be larger than or equal to 1'
+    }});
+  }
+  const db = await connection();
+  await db.collection('products').updateOne({_id: ObjectId(params.id)}, {
+    $set: {
+      name: body.name,
+      quantity: body.quantity
+    }
+  });
+  const isFound = await db.collection('products').findOne({ 'name': body['name']});
+  return res.status(OK).json(isFound);
+};
+
+app.put('/products/:id', rescue(async (req, res) => {
+  const end = await updateProduct(req, res);
   return end;
 }));
 
