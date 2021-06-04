6f53faea43cb891d325c2797d1a32cda06563413
Fabio Ferreira <fabio35547@gmail.com>
2021-06-04 18:40:26 -0300

    Requisito 9

--- services/saleService.js
+++ services/saleService.js
@@ -1,10 +1,26 @@
 const SaleModel = require('../models/saleModel');
 const SaleValidation = require('./saleValidation');
+const ProductModel = require('../models/produtcModel');
+
+const updateStock = async (products, verb) => {
+  for (const product of products) {
+    const productFound = await ProductModel.findById(product.productId);
+    if (verb === 'delete') {
+      productFound.quantity += product.quantity;
+    } else if (verb === 'post') {
+      productFound.quantity -= product.quantity;
+    }
+    const { _id, name, quantity } = productFound;
+    await ProductModel.update(_id, { name, quantity });
+  }
+};
 
 const create = async (products) => {
   await SaleValidation.prductsValidations(products);
 
   const sale = await SaleModel.create(products);
+
+  await updateStock(products, 'post');
 
   return sale;
 };
@@ -38,6 +54,10 @@ const exclude = async (id) => {
   SaleValidation.validateUpdateSaleId(id);
   
   const SaleDataDeleted = await SaleModel.exclude(id);
+
+  const { itensSold } = SaleDataDeleted;
+
+  await updateStock(itensSold, 'delete');
   
   return SaleDataDeleted;
 };
