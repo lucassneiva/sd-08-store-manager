495f8dd3929fdde944e53dbf52aacc809d355250
hugoleonardodev <hugoleonardo.dev@gmail.com>
2021-06-05 17:59:26 -0300

    fix PORT listening

--- models/connection.js
+++ models/connection.js
@@ -1,3 +1,4 @@
+// require('dotenv').config();
 const { MongoClient } = require('mongodb');
 
 const OPTIONS = {
@@ -5,10 +6,13 @@ const OPTIONS = {
   useUnifiedTopology: true,
 };
 
+const DB_URL = 'mongodb://localhost:27017/StoreManager';
+const DB_NAME = 'StoreManager';
+
 const connection = async () => {
   return MongoClient
-    .connect(process.env.DB_URL, OPTIONS)
-    .then((conn) => conn.db(process.env.DB_NAME))
+    .connect(DB_URL, OPTIONS)
+    .then((conn) => conn.db(DB_NAME))
     .catch((err) => {
       console.error(err);
       process.exit(1);
