5cc89654cb44e45c4be8f4c0a342ec38c0d0465b
Edson Araujo <araujo.barros@matematica.ufrj.br>
2021-06-05 17:46:57 -0300

    Requisito 8

--- index.js
+++ index.js
@@ -44,7 +44,6 @@ const addSale = async (req, res) => {
   }
   const db = await connection();
   const insertion = await db.collection('sales').insertOne({'itensSold': body});
-  console.log(insertion.ops[0]);
   return res.status(OK).json(insertion.ops[0]);
 };
 
@@ -53,46 +52,7 @@ app.post('/sales', rescue(async (req, res) => {
   return end;
 }));
 
-// 6 - Crie um endpoint para listar as vendas
 
-const getSales = async (idParam) => {
-  const db = await connection();
-  const sales = await db.collection('products').findOne(ObjectId(idParam));
-  return sales;
-};
-
-const findSales = async (req, res) => {
-  const { params } = req;
-  if (params.id.length !== ID_LENGTH) {
-    return res.status(NOT_FOUND).json({err: {
-      code: 'not_found',
-      message: 'Sale not found'
-    }});
-  }
-  const sales = await getSales(params.id);
-  return res.status(OK).json(sales);
-};
-
-app.get('/sales/:id', rescue(async (req, res) => {
-  const end = await findSales(req, res);
-  return end;
-}));
-
-const getAllSales = async () => {
-  const db = await connection();
-  const allSales = await db.collection('sales').find().toArray();
-  return allSales;
-};
-
-const findAllSales = async (res) => {
-  const allSales = await getAllSales();
-  return res.status(OK).json({sales: allSales});
-};
-
-app.get('/sales', rescue(async (_req, res) => {
-  const end = await findAllSales(res);
-  return end;
-}));
 
 // 7 - Crie um endpoint para atualizar uma venda
 
@@ -119,5 +79,84 @@ const updateSales = async (req, res) => {
 
 app.put('/sales/:id', rescue(async (req, res) => {
   const end = await updateSales(req, res);
+  return end;
+}));
+
+// 8 - Crie um endpoint para deletar uma venda
+
+const deleteSales = async (idParam) => {
+  const db = await connection();
+  await db.collection('sales').deleteOne({ _id: ObjectId(idParam) });
+};
+
+const findToDelete = async (req, res) => {
+  const { params, body } = req;
+  if (params.id.length !== ID_LENGTH) {
+    return res.status(UNPROCESSABE_ENTITY).json({err: {
+      code: 'invalid_data',
+      message: 'Wrong sale ID format'
+    }});
+  }
+  await deleteSales(params.id);
+  return res.status(OK).json(body);
+};
+
+app.delete('/sales/:id', rescue(async (req, res) => {
+  const end = await findToDelete(req, res);
+  return end;
+}));
+
+// 6 - Crie um endpoint para listar as vendas
+
+const existSale = async (res, idParam) => {
+  if (idParam !== ID_LENGTH ) {
+    return res.status(NOT_FOUND).json({err: {
+      code: 'not_found',
+      message: 'Sale not found'
+    }});
+  }
+  const db = await connection();
+  const sale = await db.collection('sales').findOne({ _id: ObjectId(idParam) });
+  console.log('existSale', sale);
+  return sale;
+};
+
+// const getSales = async (idParam) => {
+//   const db = await connection();
+//   const sales = await db.collection('products').findOne(ObjectId(idParam));
+//   return sales;
+// };
+
+const findSales = async (req, res) => {
+  const { params } = req;
+  const sale = await existSale(res, params.id);
+  if (!sale) {
+    return res.status(NOT_FOUND).json({err: {
+      code: 'not_found',
+      message: 'Sale not found'
+    }});
+  }
+  // const sales = await getSales(params.id);
+  return res.status(OK).json(sales);
+};
+
+app.get('/sales/:id', rescue(async (req, res) => {
+  const end = await findSales(req, res);
+  return end;
+}));
+
+const getAllSales = async () => {
+  const db = await connection();
+  const allSales = await db.collection('sales').find().toArray();
+  return allSales;
+};
+
+const findAllSales = async (res) => {
+  const allSales = await getAllSales();
+  return res.status(OK).json({sales: allSales});
+};
+
+app.get('/sales', rescue(async (_req, res) => {
+  const end = await findAllSales(res);
   return end;
 }));
