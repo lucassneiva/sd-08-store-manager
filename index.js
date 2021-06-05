fcc941a60f2247a907bda3a25acc84654c9ba466
hugoleonardodev <hugoleonardo.dev@gmail.com>
2021-06-05 17:09:45 -0300

    fix PORT listening

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const errorMiddleware = require('./middlewares/error');
 const app = express();
 
 const PORT_3000 = 3000;
+const DB_PORT = 27017;
 
 app.use(bodyParser.json());
 // não remova esse endpoint, e para o avaliador funcionar
@@ -21,6 +22,6 @@ app.post('/products', products.createOne);
 app.use(errorMiddleware);
 
 app.listen(
-  process.env.PORT || PORT_3000,
-  () => console.log(`Server listening on port ${process.env.PORT || PORT_3000}`)
+  process.env.PORT || DB_PORT,
+  () => console.log(`Server listening on port ${process.env.PORT || DB_PORT}`)
 );
