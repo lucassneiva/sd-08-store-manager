377906ca9311bfcb07c366b0bfca95e7ec140ce3
Silvio Dayube <scarige@gmail.com>
2021-06-06 20:03:56 -0300

    feat: Add utility files

---
+++ utils/generateError.js
@@ -0,0 +1,13 @@
+const HTTP = require('./httpStatusCodes');
+
+module.exports = (message, status = HTTP.UNPROCESSABLE) => {
+  let code;
+
+  /* prettier-ignore */
+  switch (status) {
+  case HTTP.UNPROCESSABLE:
+    code = 'invalid_data';
+  }
+
+  return { err: { code, message } };
+};
