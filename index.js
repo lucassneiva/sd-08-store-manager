ac19c11f1da860ec356f5fa65248f440c038a5d5
Silvio Dayube <scarige@gmail.com>
2021-06-06 20:13:12 -0300

    feat: Update main index and add creation endpoint

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+const express = require('express');
+const products = require('./controllers/productController');
+
+const DEFAULT_PORT = 3000;
+const PORT = process.env.PORT || DEFAULT_PORT;
+
+const app = express();
+app.use(express.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.post('/products', products.create);
+
+app.listen(PORT, () => console.log(`Listening to port ${PORT}`));
