99b99e8cdc654ca11beb3e9cc181cb386b7157ed
JRGCast <joao.ralph1@gmail.com>
2021-06-07 18:00:47 -0300

    refatorada connection com .then

--- models/connection.js
+++ models/connection.js
@@ -1,28 +1,22 @@
 const { MongoClient } = require('mongodb');
 
-const DB_NAME = 'StoreManager';
-
-// para o avaliador
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
-
-// para o local
-const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
-
 const OPTIONS = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
 
-let db = null;
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+// const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+const DB_NAME = 'StoreManager';
 
-module.exports = async () => {
-  if (db) return db;
-  try {
-    const connection = await MongoClient.connect(MONGO_DB_URL, OPTIONS);
-    db = await connection.db(DB_NAME);
-    return db;
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
+const connection = () => { // depois tentar com o async await
+  return MongoClient
+    .connect(MONGO_DB_URL, OPTIONS)
+    .then((conn) => conn.db(DB_NAME))
+    .catch((err) => {
+      console.error(err);
+      process.exit(1);
+    });
 };
+
+module.exports = connection;
