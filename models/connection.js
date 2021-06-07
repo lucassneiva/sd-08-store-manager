357347700a97fe04349534bafea93f8aac38de92
Silvio Dayube <scarige@gmail.com>
2021-06-07 02:19:14 -0300

    fix: Change mongo url

--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb');
 
-const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager'; // usar no db local
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager'; // usar no avaliador
+// const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager'; // usar no db local
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager'; // usar no avaliador
 
 const DB_NAME = 'StoreManager';
 const OPTIONS = {
