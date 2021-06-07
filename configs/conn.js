0eded6c0cc5c721451e3276c824238cbb216e578
Uilton Rocha <itoclick@gmail.com>
2021-06-07 16:22:17 -0300

    estruturando

--- configs/conn.js
+++ configs/conn.js
@@ -7,15 +7,15 @@ const DB_NAME = 'StoreManager';
 const connection = () => 
   MongoClient.connect(MONGO_DB_URL, {
     useNewUrlParser:true, // toda conexão é identificada ao usar a mesma url 
-    userUnifiedTopology: true, // toda aconexão tem a mesma topologia/caminho
-  }).then(
-    conn.db(DB_NAME)
+    useUnifiedTopology: true, // toda aconexão tem a mesma topologia/caminho
+  }).then((conn) => conn.db(DB_NAME)
   ).catch(
     err => {
       console.error(err);
       process.exit(1);
     }
   );
+connection();
 // Para o avaliador funcionar altere a conexão do banco para:
 
 // const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
--- models/productModel.js
+++ models/productModel.js
@@ -1,12 +1,16 @@
 const { ObjectId } = require('mongodb');
 const conn = require('../configs/conn');
 
-const getAll = async() => {
-  conn().then(db => db.collection('products').find().toArray());
+const getAll = async(_req, res) => {
+  const status = 200;
+  const result = conn().then(db => db.collection('products').find().toArray());
+  res.status(status).send(result);
 };
 
 const insertProduct = async(req, res) => {
-  conn().then(db => db.collection('products').insert(req.body).toArray());
+  const status = 200;
+  conn().then(db => db.collection('products').insert(req.body));
+  res.status(status).send(ops.ObjectId);
 };
 
 
