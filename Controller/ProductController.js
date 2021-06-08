73ac43027d63092dacbcdccef7046dec22fd214c
Galaraz <jvictor.si.2013@gmail.com>
2021-06-07 20:33:46 -0400

    mais uma corrigido

--- Controller/ProductController.js
+++ Controller/ProductController.js
@@ -1,8 +1,8 @@
 const ProductService = require('../Service/ProductService');
 
-const {StatusCodes} = require('http-status-codes');
+const { StatusCodes } = require('http-status-codes');
 
-const createProduct = async (req, res ) => {
+const createProduct = async (req, res) => {
   console.log('[PRODUCTS CONTROLLER] : CHAMOU O MÉTODO ADICIONAR UM PRODUCTS');
   try {
     const { name, quantity } = req.body;
@@ -11,7 +11,6 @@ const createProduct = async (req, res ) => {
     if (result.isError) return res.status(result.status).json(result);
 
     return res.status(StatusCodes.CREATED).json(result);
-
   } catch (error) {
     console.log(`[PRODUCTS CONTROLLER] : buscar => ${error}`);
     res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: error.message });
@@ -20,59 +19,50 @@ const createProduct = async (req, res ) => {
 const getAllProducts = async (_req, res) => {
   console.log('[PRODUCTS CONTROLLER] : CHAMOU O MÉTODO BUSCAR TODOS PRODUCTS');
   try {
-  const result = await ProductService.getAllProducts();
-   
-  return res.status(StatusCodes.OK).json(result);
+    const result = await ProductService.getAllProducts();
 
- 
-} catch (error) {
-  console.log(`[PRODUCTS CONTROLLER] : buscar => ${error}`);
-  res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: error.message });
-}
-};
-
-const getOneProduct = async (req, res) => {
-  console.log('[PRODUCTS CONTROLLER] : CHAMOU O MÉTODO BUSCAR UM PRODUCTO');
- try {
-  const {id} = req.params
-  const result = await ProductService.getProductId(id);
-  if (result.isError) return res.status(result.status).json(result);
-  return res.status(StatusCodes.OK).json(result);
- } catch (error) {
-  
-  console.log(`[PRODUCTS CONTROLLER] : buscar => ${error}`);
-  res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: error.message });
- }
-  
-
-};
-const deleteProduct = async (req, res) => {
-  console.log('[PRODUCTS CONTROLLER] : CHAMOU O MÉTODO DELETAR UM PRODUCT');
-  const {id}= req.params
-  try {
-    const product = await ProductService.deleteProduct(id);
-    res.status(StatusCodes.NO_CONTENT).json( product);
-    
+    return res.status(StatusCodes.OK).json(result);
   } catch (error) {
     console.log(`[PRODUCTS CONTROLLER] : buscar => ${error}`);
     res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: error.message });
   }
- 
 };
 
-const updateProduct= async (req, res) =>{
+const getOneProduct = async (req, res) => {
+  console.log('[PRODUCTS CONTROLLER] : CHAMOU O MÉTODO BUSCAR UM PRODUCTO');
+  try {
+    const { id } = req.params;
+    const result = await ProductService.getProductId(id);
+    if (result.isError) return res.status(result.status).json(result);
+    return res.status(StatusCodes.OK).json(result);
+  } catch (error) {
+    console.log(`[PRODUCTS CONTROLLER] : buscar => ${error}`);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: error.message });
+  }
+};
+const deleteProduct = async (req, res) => {
+  console.log('[PRODUCTS CONTROLLER] : CHAMOU O MÉTODO DELETAR UM PRODUCT');
+  const { id } = req.params;
+  try {
+    const product = await ProductService.deleteProduct(id);
+    res.status(StatusCodes.NO_CONTENT).json(product);
+  } catch (error) {
+    console.log(`[PRODUCTS CONTROLLER] : buscar => ${error}`);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: error.message });
+  }
+};
+
+const updateProduct = async (req, res) => {
   console.log('[PRODUCTS CONTROLLER] : CHAMOU O MÉTODO EDITAR UM PRODUCT');
- try {
-  const {id}= req.params
-  const { name, quantity } = req.body;
-  const product = await ProductService.updateProduct(id,name , quantity)
-  res.status(StatusCodes.OK).json(product);
- } catch (error) {
-  console.log(`[PRODUCTS CONTROLLER] : buscar => ${error}`);
-  res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: error.message });
- }
-  
+  try {
+    const { id } = req.params;
+    const { name, quantity } = req.body;
+    const product = await ProductService.updateProduct(id, name, quantity);
+    res.status(StatusCodes.OK).json(product);
+  } catch (error) {
+    console.log(`[PRODUCTS CONTROLLER] : buscar => ${error}`);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: error.message });
+  }
 };
 
-
-module.exports = { createProduct, getAllProducts, getOneProduct,deleteProduct,updateProduct};
+module.exports = { createProduct, getAllProducts, getOneProduct, deleteProduct, updateProduct };
