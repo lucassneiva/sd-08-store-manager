e6bea4d405d3bb317bd13043c86835fc0c9b3620
Gustavo Thirion - Turma 8 <gustavo.fthirion@gmail.com>
2021-06-09 02:55:32 -0300

    requisito 1 - cria rota para criarum novo produto no banco de dados - mongodb://mongodb:27017/StoreManager - correcoes connection.js

--- models/connection.js
+++ models/connection.js
@@ -1,17 +1,22 @@
 const { MongoClient } = require('mongodb');
 
-const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
-const DB_NAME = 'StoreManager';
+const OPTIONS = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
 
-const connection = () => MongoClient
-  .connect(MONGO_DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then((conn) => conn.db(DB_NAME))
-  .catch((err) => {
-    console.error(err);
-    process.exit(1);
-  });
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+
+let db = null;
+
+const connection = async () => {
+  return db
+    ? Promise.resolve(db)
+    : MongoClient.connect(MONGO_DB_URL, OPTIONS)
+      .then((conn) => {
+        db = conn.db('StoreManager');
+        return db;
+      });
+};
 
 module.exports = connection;
