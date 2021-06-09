b2ebcbf6baeb34124979885c8199691d584a9373
Gustavo Thirion - Turma 8 <gustavo.fthirion@gmail.com>
2021-06-09 02:47:25 -0300

    requisito 1 - cria rota para criarum novo produto no banco de dados - mongodb://mongodb:27017/StoreManager

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
 
-const MONGO_DB_URL = 'mongodb://mongodb:27017';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
 const DB_NAME = 'StoreManager';
 
 const connection = () => MongoClient
