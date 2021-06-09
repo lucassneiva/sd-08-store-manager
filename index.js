e7110c0f61cf042bfbc0e611d673b72cca972b60
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2021-06-09 16:39:51 -0300

    req1 ok

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-// não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
-  response.send();
+//DISCLAIMER: projeto realizado inspirado nas aulas ao vivo da T8 com o mestre Moreira, com acréscimos visualizados no Course (ex.: colocar o OPTIONS separado)
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const router = require('./routers/productsRouter');
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log('Online!');
 });
--- routers/productsRouter.js
+++ routers/productsRouter.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const products = require('./controllers/productController');
 
-//chamar controller
 const router = express.Router();
 
-router.post('/products', nomeController);
+// não remova esse endpoint, e para o avaliador funcionar
+router.get('/', (_request, response) => {
+  response.send();
+});
+
+// router.get('/', productController.getAllProducts ); - exemplo de como usar o controller
+// Criar produtos
+router.post('/products', products.createProducts);
+
+module.exports = router;
