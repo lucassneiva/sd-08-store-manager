f5ce2b92fdd8a2deb3942a33a88fba296140da7e
Rita Jeveaux <ritajeveaux@gmail.com>
2021-06-09 15:32:04 -0300

    CorrigindoURL

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const products = require('./controllers/products');
-// const sales = require('./controllers/Sales');
 
 const app = express();
 const PORT = '3000';
