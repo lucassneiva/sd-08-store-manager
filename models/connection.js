12b5c2ad8af76b1c9736ebac054989a237c1f981
caiomzrocha <caiomzrocha@gmail.com>
2021-06-10 19:41:23 -0300

    [INIT] Cria o arquivo models/connection.js para estabelecer a conexão com o banco de dados.

---
+++ models/connection.js
@@ -0,0 +1,23 @@
+const { MongoClient } = require('mongodb');
+
+const DB_NAME = 'StoreManager';
+// const MONGODB_URL = 'mongodb://mongodb:27017/StoreManager';
+const MONGODB_URL = 'mongodb://localhost:27017/StoreManager';
+const OPTIONS = {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+};
+
+let db = null;
+const connection = () => {
+	if (db) {
+		return Promise.resolve(db);
+	}
+	return MongoClient.connect(MONGODB_URL, OPTIONS)
+		.then((conn) => {
+			db = conn.db(DB_NAME);
+			return db;
+		});
+};
+
+module.exports = connection;
