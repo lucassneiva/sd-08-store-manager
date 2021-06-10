20c851ddf82fa44bc33c6805c89443b72b7e49f2
devFelipeFranca <felipefrancafj@gmail.com>
2021-06-10 15:05:58 -0300

    Update index

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const app = express();
+app.use(bodyParser.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+
+// REQ - 01 | Cadastro e Pesquisa de Produtos
+app.post('/product/');
+app.get('/product/:name');
