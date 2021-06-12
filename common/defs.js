f2ee63f277d9052dcffffb519d4d7090d54f6321
devFelipeFranca <felipefrancafj@gmail.com>
2021-06-12 17:24:26 -0300

    Hum... Ok

--- common/defs.js
+++ common/defs.js
@@ -1,4 +1,5 @@
-const URL_MONGO_DATABASE = 'mongodb://127.0.0.1:27017/StoreManager';
+const URL_MONGO_DATABASE_LOCAL = 'mongodb://127.0.0.1:27017/StoreManager';
+const URL_MONGO_DATABASE = 'mongodb://mongodb:27017/StoreManager';
 const NAME_OF_DATABASE = 'StoreManager';
 const OPTIONS = {
   useNewUrlParser: true,
@@ -9,6 +10,7 @@ const MIN_LENGTH = 5;
 const MIN_QUANTITY = 0;
 
 module.exports = {
+  URL_MONGO_DATABASE_LOCAL,
   URL_MONGO_DATABASE,
   NAME_OF_DATABASE,
   DEFAULT_PORT,
