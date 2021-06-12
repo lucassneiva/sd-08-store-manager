b47587d59cdb2668800e9bf1b201fe68295506db
caiomzrocha <caiomzrocha@gmail.com>
2021-06-11 22:49:40 -0300

    [REQ-01][FIX] Conserta os tabs do arquivo connections.js.

--- models/connection.js
+++ models/connection.js
@@ -4,21 +4,21 @@ const DB_NAME = 'StoreManager';
 const MONGODB_URL = 'mongodb://mongodb:27017/StoreManager';
 // const MONGODB_URL = 'mongodb://localhost:27017/StoreManager';
 const OPTIONS = {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 };
 
 let db = null;
 const connection = () => {
-	if (db) {
-		return Promise.resolve(db);
-	}
+  if (db) {
+    return Promise.resolve(db);
+  }
 
-	return MongoClient.connect(MONGODB_URL, OPTIONS)
-		.then((conn) => {
-			db = conn.db(DB_NAME);
-			return db;
-		});
+  return MongoClient.connect(MONGODB_URL, OPTIONS)
+    .then((conn) => {
+      db = conn.db(DB_NAME);
+      return db;
+    });
 };
 
 module.exports = connection;
