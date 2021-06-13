6213a7fde22f08d288b9cbfa254cf671447bb352
Doug-Fernando <doug.fernando95@gmail.com>
2021-06-13 19:12:19 -0300

    feat: adiciona teste na função "create" do service "products"

--- test/unit/services.js
+++ test/unit/services.js
@@ -1,8 +1,40 @@
 const { expect } = require("chai")
 
-describe('testando', () => {
-  it('teste', () => {
-    const show = 2;
-    expect(show).to.be.equals(2);
+const ProductsService = {
+  create: () => {},
+}
+
+describe('A camada service valida os campos corretamente ao inserir um produto', () => {
+  describe('Quando insere com os dados inválidos', () => {
+    const payload = {
+      name: 'Pro',
+      quantity: 0,
+    };
+
+    it('retorna um objeto', async () => {
+      const response = await ProductsService.create(payload);
+      expect(response).to.be.an('object');
+    });
+
+    it('retorna um objeto com a propriedade e valor "error: true"', async () => {
+      const response = await ProductsService.create(payload);
+      expect(response.error).to.be.equals(true);
+    });
+  })
+
+  describe('Quado insere com os dados válido', () => {
+    const payload = {
+      name: 'Produto do service para teste',
+      quantity: 1
+    };
+    const ID_EXAMPLE = '604cb554311d68f491ba5781';
+    it('retorna um objeto', async () => {
+      const response = await ProductsService(payload);
+      expect(response).to.be.an('object');
+    })
+    it('retorna um objeto com a propriedade "_id"', async () => {
+      const response = await ProductsService(payload);
+      expect(response).to.have.property('_id');
+    })
   })
 })
