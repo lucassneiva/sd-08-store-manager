219a0d143b2b9ec5eef2cc0c051732f6b02c52f7
lucasbgama <lucasbg@protonmail.com>
2021-06-13 18:50:53 -0300

    requisito 3

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,16 +1,43 @@
 const express = require('express');
-const { getProducts } = require('../models/productsModel');
+const { getProducts, validateId, editProduct } = require('../models/productsModel');
 const { addProduct, getProductById } = require('../services/productsService');
 const router = express.Router();
 
-const GET_STATUS = 200;
+const SUCCESS = 200;
 const CREATE_STATUS = 201;
 const INVALID_DATA = 422;
 const SERVER_ERROR = 500;
 const INVALID_DATA_CODE = 'invalid_data';
 
+const validateIdMiddleware = (req, res, next) => {
+  const { id } = req.params;
+  if(!validateId(id)) {
+    return res.status(INVALID_DATA)
+      .json({err: {code: INVALID_DATA_CODE, message: 'Wrong id format'}});
+  }
+  next();
+};
+
+const validateDataMiddleWare = (req, res, next) => {
+  const { name, quantity } = req.body;
+  const MIN_NAME_LENGTH = 5;
+  let err = '';
+  if(name.length < MIN_NAME_LENGTH) {
+    err = '"name" length must be at least 5 characters long';
+  }
+  if(typeof quantity === 'string') {
+    err = '"quantity" must be a number';
+  }
+  if(quantity < 1) {
+    err = '"quantity" must be larger than or equal to 1';
+  }
+  if(err) return res.status(INVALID_DATA)
+    .json({err: {code: INVALID_DATA_CODE, message: err}});
+  next();
+};
+
 router.route('/')
-  .post(async (req, res) => {
+  .post(validateDataMiddleWare, async (req, res) => {
     try {
       const data = req.body;
       const response = await addProduct(data);
@@ -26,26 +53,40 @@ router.route('/')
   .get(async (_req, res) => {
     try { 
       const response = await getProducts();
-      res.status(GET_STATUS).json({'products': response});
+      res.status(SUCCESS).json({'products': response});
     } catch(err) {
       console.error(err);
       res.status(SERVER_ERROR).send('Error');
     }
   });
 
-router.get('/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const response = await getProductById(id);
-    if(response instanceof Error) {
-      return res.status(INVALID_DATA)
-        .json({err: {code: INVALID_DATA_CODE, message: response.message}});
+
+router.route('/:id')
+  .all(validateIdMiddleware)
+  .get(async (req, res) => {
+    try {
+      const { id } = req.params;
+      const response = await getProductById(id);
+      if(response instanceof Error) {
+        return res.status(INVALID_DATA)
+          .json({err: {code: INVALID_DATA_CODE, message: response.message}});
+      }
+      res.status(SUCCESS).json(response);
+    } catch(err) {
+      console.error(err);
+      res.status(SERVER_ERROR).send('Error.');
     }
-    res.status(GET_STATUS).json(response);
-  } catch(err) {
-    console.error(err);
-    res.status(SERVER_ERROR).send('Error.');
-  }
-});
+  })
+  .put(validateDataMiddleWare, async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { name, quantity } = req.body;
+      await editProduct(id, name, quantity);
+      res.status(SUCCESS).json({_id: id, name, quantity});
+    } catch(err) {
+      console.error(err);
+      res.status(SERVER_ERROR).send('Error.');
+    }
+  });
 
 module.exports = router;
--- models/productsModel.js
+++ models/productsModel.js
@@ -1,3 +1,4 @@
+const { ObjectID, ObjectId } = require('bson');
 const connect = require('./mongoConnection');
 
 collection = 'products';
@@ -10,18 +11,28 @@ const getProducts = async () => {
   return connect().then((db) => db.collection(collection).find().toArray());
 };
 
-const getProductById = async (_id) => {
+const getProductById = async (id) => {
   return connect()
-    .then((db) => db.collection(collection).findOne({ _id }));
+    .then((db) => db.collection(collection).findOne({ _id: ObjectId(id) }));
 };
 
 const createProduct = async (name, quantity) => connect()
   .then((db) => db.collection(collection).insertOne({name, quantity}))
   .then((result) => result.ops[0]);
 
+const validateId = (id) => ObjectID.isValid(id);
+
+const editProduct = async (id, name, quantity) => {
+  return connect()
+    .then((db) => db.collection(collection)
+      .updateOne({ _id: ObjectId(id) }, { $set: { name, quantity }}));
+};
+
 module.exports = {
   createProduct,
   getProductByName,
   getProducts,
-  getProductById
+  getProductById,
+  validateId,
+  editProduct
 };
