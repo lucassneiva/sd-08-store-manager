dde5762f4fad854a16191727cec3c98cc054403b
caiomzrocha <caiomzrocha@gmail.com>
2021-06-18 19:36:59 -0300

    [FIX] Conserta o erro ao enviar ids repetidos na mesma venda.

--- middlewares/idValidationInArray.js
+++ middlewares/idValidationInArray.js
@@ -5,46 +5,26 @@ const UNPROCESSABLE_ENTITY_STATUS = 422;
 const idValidationInArray = async (req, res, next) => {
   const array = req.body;
   const productIds = array.map((id) => id.productId);
-  const products = await getProductsByIds(productIds);
+  const uniqueProductIds = productIds
+    .filter((id, index, arr) => index === arr.indexOf(id));
 
-  // const invalidId = array.find(({ productId }) => {
-  //   const res = allProducts.find(({ _id: id }) => {
-  //     const resu = id.toString() === productId;
-  //     return resu;
-  //   });
-  //   return !res;
-  // });
-  // console.log('invalidId', invalidId);
+  const products = await getProductsByIds(uniqueProductIds);
 
-  // const invalidID = array.filter(
-  //   async ({ productId }) => {
-  //     const isValidId = ObjectId.isValid(productId);
-  //     const product = await getProductById(ObjectId(productId));
-  //     // console.log('productId', productId, typeof productId);
-  //     // console.log('isValidId', isValidId, typeof isValidId);
-  //     // console.log('product', product, typeof product);
-  //     const res = !(isValidId && product);
-  //     console.log('res', res, typeof res);
-  //     return res;
-  //   }
-  // );
-
-  // console.log('invalidID', invalidID, typeof invalidID);
-  // console.log('invalidID', invalidID, typeof invalidID);
-  // console.log('invalidID', invalidID, typeof invalidID);
-  // console.log('invalidID', invalidID, typeof invalidID);
-
-  // console.log('invalidIDs', invalidIDs, 'aqui');
-  if (productIds.length !== products.length) {
+  if (uniqueProductIds.length !== products.length) {
     return res.status(UNPROCESSABLE_ENTITY_STATUS).json({
       err: {
         code: 'invalid_data',
         message: 'Wrong product ID or invalid quantity',
+        allProducts,
+        body: array,
       },
     });
   }
-
   return next();
 };
 
 module.exports = idValidationInArray;
+
+// referência para remover elementos duplicados de um array
+// usada para atribuir o valor à constante uniqueProductIds
+// https://net2.com/how-to-remove-duplicates-from-arrays-in-javascript/
