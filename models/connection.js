54060cd44fbfd70459440a90f253a027435cac8d
lucasvr39 <lucasveras1994@gmail.com>
2021-06-20 19:59:21 -0300

    fix connection file and func to evaluator

--- models/connection.js
+++ models/connection.js
@@ -6,23 +6,24 @@ const DB_NAME = 'StoreManager';
 // para o avaliador funcionar
 const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
 
-const OPTIONS = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-};
+// const OPTIONS = {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// };
 
-let db = null;
+// let db = null;
 
 const connection = () => {
-  return db
-    ? Promise.resolve(db)
-    : MongoClient.connect(MONGO_DB_URL, OPTIONS)
-      .then((conn) => {
-        db = conn.db(DB_NAME);
-        return db;
-      });
+  return MongoClient
+    .connect(MONGO_DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    .then((conn) => conn.db(DB_NAME))
+    .catch((err) => {
+      console.error(err);
+      process.exit();
+    });
 };
 
 module.exports = connection;
-
-
