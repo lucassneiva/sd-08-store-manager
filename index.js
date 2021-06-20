bb0f3bf8a378a939c05a5f61c9e9d9626bcc074f
joao <joaocarlitoneto@gmail.com>
2021-06-20 10:58:29 -0300

    Requisitos 1 ao 44 --- Ate que enfim eu entendi

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+const express = require('express');
+const app = express();
+const ProductController = require('./src/controllers/productController');
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.get('/products', ProductController.getAll);
+app.get('/products/:id', ProductController.findById);
+app.post('/products', ProductController.create);
+app.put('/products/:id', ProductController.update);
+app.delete('/products/:id', ProductController.exclude);
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`## SERVIDOR RODANDO NA PORTA ${PORT} ##`));
